44c12b2ae0172f2fbc2758fb7b8d29a2c0a2cd8f
Matthew Hemmings <discount.yoyos@gmail.com>
2017-01-24 11:16:01 -0800

    Updated goOffline function of l2lchannel to correctly check for master status before killing it

--- l2lchannel.js
+++ l2lchannel.js
@@ -45,25 +45,32 @@ export class Channel {
   goOffline() {
     console.log(this)
     this.online = false;
-    var status = {senderRecvrA : null,senderRecvrB : null}
+    
+    var l2lA = this.senderRecvrA.l2lclient,
+    l2lB = this.senderRecvrB.l2lclient
+    if(!l2lA){ console.log('l2lA Missing');return''}
+    if(!l2lB){ console.log('l2lB Missing');return''}
+    l2lA.sendTo(l2lA.trackerId,'leaveRoom',{roomName: l2lB.socketId.split('#')[1]})
+    
     if (this.senderRecvrA && this.senderRecvrA.l2lclient) {
       this.senderRecvrA.l2lclient.remove()
       console.log('senderRecvrA disconnected')
-      status.senderRecvrA = 'offline'
-    } else {
-      status.senderRecvrA = 'online'
-      console.log('senderRecvrA not disconnected');
       
+    } else {      
+      console.log('senderRecvrA not disconnected');      
     }
+    
     if (this.senderRecvrB && this.senderRecvrB.l2lclient) {
-      this.senderRecvrB.l2lclient.remove()
-      console.log('senderRecvrB disconnected')
-      status.senderRecvrB = 'offline'
-    } else {
-      console.log('senderRecvrB not disconnected');
-      status.senderRecvrB = 'online'
+      this.getSessions(this.senderRecvrA,this.senderRecvrB,(a)=> {
+        if(a.data.length <= 1){
+          l2lB.sendTo(l2lB.trackerId,'leaveRoom',{roomName: l2lB.socketId.split('#')[1]})  
+          l2lB.remove();          
+        } else {
+          console.log('senderRecvrB not disconnected');
+        }    
+      })
     }
-    return status
+    
   }
 
   async goOnline() {    
